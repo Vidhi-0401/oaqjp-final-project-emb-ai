8bbe726365fbf8060824df90749f4194fe374599
Vidhi-0401 <patelvidhi074@gmail.com>
2026-03-04 11:04:12 -0500

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,23 +2,6 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-
-    headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
-    }
-
-    myobj = {
-         "raw_document": { "text": text_to_analyse }
-    }
-
-    response = requests.post(url, headers=headers, json=myobj)
-
-    return response.text
-import requests
-import json
-
-def emotion_detector(text_to_analyze):
 
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
@@ -26,12 +9,31 @@ def emotion_detector(text_to_analyze):
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
 
-    myobj = {
+    input_json = {
         "raw_document": {
             "text": text_to_analyze
         }
     }
 
-    response = requests.post(url, headers=headers, json=myobj)
+    response = requests.post(url, headers=headers, json=input_json)
 
-    return response.text
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
